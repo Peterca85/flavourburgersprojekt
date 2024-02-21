7c90fc05bdc4ab821e9006d518216f670352c205
Peterca85 <peter-carlsson@hotmail.com>
2024-02-21 15:16:39 +0100

    start building Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Logo from "../assets/Flavour Burgers .png";
+import {Link} from "react-router-dom"
 
 function Navbar() {
   return (
     <div className='navbar'>
       <div className='leftSide'></div>
       <img src={Logo}/>
-      <div className='rightSide'></div>
+      <div className='rightSide'>
+        <Link to="/"> Home </Link> 
+        <Link to="/menu"> Menu </Link> 
+        <Link to="/about"> About </Link> 
+        <Link to="/contact"> Contact </Link> 
+        </div>
     </div>
   )
 }
