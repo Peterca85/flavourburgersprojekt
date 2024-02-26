5dea336ded7098bc86615c4b7647e233d9816127
Peterca85 <peter-carlsson@hotmail.com>
2024-02-26 10:50:20 +0100

    video startsida

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,16 @@
 import React from "react"
 import {Link} from "react-router-dom"
-import BannerImage from "../assets/hamburgerBackground.jpg";
+import videoBg from "../assets/bgvideo.mp4";
 import "../styles/Home.css";
 
 function Home() {
   return (
-    <div className="home"  style={ {backgroundImage: `url(${BannerImage})` }}>
-      <div className='headerContainer'> 
+    <div className="home"> 
+    <div> className="overlay"</div> 
+    <video src={videoBg} autoPlay loop muted /> 
+      <div className='content'> 
       <h1> Flavour Burgers </h1>
-        <p> Bästa Burgarna i stan </p>
-        <Link to="/BestallOnline">
-        <button> BESTÄLL HÄR </button>
-        </Link>
+     <p>Bästa burgarna i stan !</p>
       </div>
     
     </div>
