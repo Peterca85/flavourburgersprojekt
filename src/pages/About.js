d2664a330e8326005d6790f405caf67103709e34
Peterca85 <peter-carlsson@hotmail.com>
2024-03-01 10:43:42 +0100

    linebreaks om oss

--- src/pages/About.js
+++ src/pages/About.js
@@ -18,25 +18,25 @@ function About() {
               Vi är inte bara en hamburgerrestaurang, vi är en
                destination för de som älskar att uppleva nya 
                smaker och njuta av högkvalitativa råvaror i varje måltid.</p>
-             
+      <br></br>
                <p>Vi på Flavour Burgers strävar efter att erbjuda mer än bara en vanlig
                 hamburgare. Vårt koncept handlar om att skapa unika smakkombinationer 
                 som stimulerar dina smaklökar och får dig att komma tillbaka efter mer.
                  Varje burgare är skapad med omsorg och expertis för att ge dig en 
                  smakupplevelse utöver det vanliga.</p>
-          
+         <br></br>
                  <p>Vi använder endast de färskaste och högsta kvalitetsingredienserna för 
                     att säkerställa att varje burgare är en fest för dina smaklökar.
                      Från saftiga köttburgare till vegetariska 
                     alternativ och specialburgare inspirerade av smaker från 
                     hela världen - vi har något för alla.</p>
-                    
+                    <br></br>
                     <p>Hos Flavour Burgers vill vi inte bara tillfredsställa din aptit, 
                         vi vill också skapa en atmosfär där du kan koppla av och njuta 
                         av varje måltid. Vår restaurang är
                          designad för att vara mysig och inbjudande, perfekt för en trevlig
                           måltid med vänner och familj. </p>
-                          
+                          <br></br>
                           <p>Vårt team består av passionerade matälskare och kockar som brinner
                             för att skapa fantastiska smakupplevelser. Vi strävar efter att leverera
                              utmärkt service och göra varje besök till Flavour Burgers till en minnesvärd
@@ -46,6 +46,7 @@ function About() {
                                 vanlig hamburgare. Vi är dedikerade till att erbjuda enastående smaker,
                                  förstklassiga råvaror och en trevlig atmosfär som får dig att vilja komma tillbaka gång på gång.
                                  Kom och upplev smaken av Flavour Burgers idag och låt oss ta dig med på en gastronomisk resa du sent kommer att glömma!</p>
+                           
       
       </div>
       </div>
