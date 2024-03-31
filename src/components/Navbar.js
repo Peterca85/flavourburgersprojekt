49af0084bcf0d1140429caa030189b1c271f64de
petercar85 <117587523+petercar85@users.noreply.github.com>
2024-03-31 12:33:25 +0200

    menu fix click

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,45 +1,37 @@
-
-import React, { useState } from 'react'
-import { useRef } from 'react';
+import React, { useState } from "react";
+import { useRef } from "react";
 import Logo from "../assets/FlavourBurgers.png";
-import {Link} from "react-router-dom"
-import ReorderIcon from '@mui/icons-material/Reorder';
-import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import { Link } from "react-router-dom";
+import ReorderIcon from "@mui/icons-material/Reorder";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 import "../styles/Navbar.css";
 
-
 function Navbar() {
+  const navRef = useRef();
 
-  const navRef= useRef();
-
-  const showNavbar = () =>{
-    navRef.current.classList.toggle("responsive_nav")
-  }
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
 
   return (
-   <header>
-    <img src={Logo} width={100} alt="logotype"/>
-    <nav ref={navRef}>
-  <Link to="/"> Hem </Link> 
-  <Link to="/meny"> Meny </Link> 
-  <Link to="/bestallOnline"> Beställ Online </Link> 
-  <Link to="/omOss"> Om oss </Link> 
-  <Link to="/happenings"> Happenings </Link> 
-  <Link to="/Kontakt"> Kontakt </Link> 
-  <button className='nav-btn nav-close-btn'
-   onClick={showNavbar}>
-  <HighlightOffIcon fontSize="large" />
-   </button>
-    </nav>
-    <button className='nav-btn' onClick={showNavbar}>
-     
-      <ReorderIcon fontSize="large"/>
-    </button>
-   </header>
-  )
+    <header>
+      <img src={Logo} width={100} alt="logotype" />
+      <nav ref={navRef}>
+        <Link to="/"> Hem </Link>
+        <Link to="/meny"> Meny </Link>
+        <Link to="/bestallOnline"> Beställ Online </Link>
+        <Link to="/omOss"> Om oss </Link>
+        <Link to="/happenings"> Happenings </Link>
+        <Link to="/Kontakt"> Kontakt </Link>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <HighlightOffIcon fontSize="large" />
+        </button>
+      </nav>
+      <button className="nav-btn" onClick={showNavbar}>
+        <ReorderIcon fontSize="large" />
+      </button>
+    </header>
+  );
 }
 
-export default Navbar
-
-
-
+export default Navbar;
