74294fb80dc4e69e26345aa9837904efb26d7468
Peterca85 <peter-carlsson@hotmail.com>
2024-04-29 13:51:14 +0200

    cvc expired form update

--- src/pages/Order/Order.js
+++ src/pages/Order/Order.js
@@ -11,14 +11,14 @@ function Order() {
   const [formData, setFormData] = useState({
     name: "",
     lastname: "",
-    email:"",
-    address:"",
-    zip:"",
-    city:"",
-    phone:"",
-    cardnumber:"",
-    expired:"",
-    cvc:"",
+    email: "",
+    address: "",
+    zip: "",
+    city: "",
+    phone: "",
+    cardnumber: "",
+    expired: "",
+    cvc: "",
   });
 
   const [formErrors, setFormErrors] = useState({});
@@ -26,9 +26,14 @@ function Order() {
   const handleChange = (event) => {
     const { name, value } = event.target;
     let newValue = value;
-    // Filtrera bort icke-siffror för specifika fält
-    if (name === "phone" || name === "zip" || name === "cardnumber" || name === "expired" || name === "cvc") {
-      newValue = value.replace(/\D/g, ''); // Tar bort icke-siffror
+    if (name === "phone" || name === "zip" || name === "cardnumber") {
+      newValue = value.replace(/\D/g, "");
+    }
+    if (name === "cvc") {
+      newValue = value.replace(/\D/g, "").slice(0, 3);
+    }
+    if (name === "expired") {
+      newValue = value.replace(/\D/g, "").slice(0, 4);
     }
     setFormData((prevState) => ({ ...prevState, [name]: newValue }));
   };
@@ -44,7 +49,6 @@ function Order() {
     setFormErrors(errors);
     if (Object.keys(errors).length === 0) {
       console.log(formData);
-      // Fortsätt med att hantera formuläret om det inte finns några fel
     }
   };
 
@@ -56,43 +60,97 @@ function Order() {
           <label>
             {" "}
             Förnamn:
-            <input type="text" placeholder="Ange Namn" name="name" value={formData.name} onChange={handleChange}></input>
-            {formErrors.name && <span className="error">{formErrors.name}</span>}
+            <input
+              type="text"
+              placeholder="Ange Namn"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+            ></input>
+            {formErrors.name && (
+              <span className="error">{formErrors.name}</span>
+            )}
           </label>
           <label>
             Efternamn:
-            <input type="text" placeholder="Ange Efternamn" name="lastname"  value={formData.lastname} onChange={handleChange}></input>
-            {formErrors.lastname && <span className="error">{formErrors.lastname}</span>}
+            <input
+              type="text"
+              placeholder="Ange Efternamn"
+              name="lastname"
+              value={formData.lastname}
+              onChange={handleChange}
+            ></input>
+            {formErrors.lastname && (
+              <span className="error">{formErrors.lastname}</span>
+            )}
           </label>
         </div>
         <label>
           Email:
-          <input type="email" placeholder="email" name="email" value={formData.email} onChange={handleChange}></input>
-          {formErrors.email && <span className="error">{formErrors.email}</span>}
+          <input
+            type="email"
+            placeholder="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          ></input>
+          {formErrors.email && (
+            <span className="error">{formErrors.email}</span>
+          )}
         </label>
         <div className="multi-fields">
           <label>
             Adress:
-            <input type="text" placeholder="Ange Adress" name="address" value={formData.address} onChange={handleChange}></input>
-            {formErrors.address && <span className="error">{formErrors.address}</span>}
+            <input
+              type="text"
+              placeholder="Ange Adress"
+              name="address"
+              value={formData.address}
+              onChange={handleChange}
+            ></input>
+            {formErrors.address && (
+              <span className="error">{formErrors.address}</span>
+            )}
           </label>
         </div>
         <div className="multi-fields">
           <label>
             Postnummer:
-            <input type="text" placeholder="Ange Postnummer" name="zip" value={formData.zip} onChange={handleChange}></input>
+            <input
+              type="text"
+              placeholder="Ange Postnummer"
+              name="zip"
+              value={formData.zip}
+              onChange={handleChange}
+            ></input>
             {formErrors.zip && <span className="error">{formErrors.zip}</span>}
           </label>
           <label>
             Stad:
-            <input type="text" placeholder="Ange Stad" name="city" value={formData.city} onChange={handleChange}></input>
-            {formErrors.city && <span className="error">{formErrors.city}</span>}
+            <input
+              type="text"
+              placeholder="Ange Stad"
+              name="city"
+              value={formData.city}
+              onChange={handleChange}
+            ></input>
+            {formErrors.city && (
+              <span className="error">{formErrors.city}</span>
+            )}
           </label>
         </div>
         <label>
           Telefon:
-          <input type="text" placeholder="Ange Telefon" name="phone" value={formData.phone} onChange={handleChange}></input>
-          {formErrors.phone && <span className="error">{formErrors.phone}</span>}
+          <input
+            type="text"
+            placeholder="Ange Telefon"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+          ></input>
+          {formErrors.phone && (
+            <span className="error">{formErrors.phone}</span>
+          )}
         </label>
       </div>
       <div className="place-order-middle">
@@ -100,20 +158,42 @@ function Order() {
         <div className="multi-fields">
           <label>
             Kortnummer:
-            <input type="text" placeholder="Ange Kortnummer" name="cardnumber" value={formData.cardnumber} onChange={handleChange}></input>
-            {formErrors.cardnumber && <span className="error">{formErrors.cardnumber}</span>}
+            <input
+              type="text"
+              placeholder="Ange Kortnummer"
+              name="cardnumber"
+              value={formData.cardnumber}
+              onChange={handleChange}
+            ></input>
+            {formErrors.cardnumber && (
+              <span className="error">{formErrors.cardnumber}</span>
+            )}
           </label>
         </div>
 
         <div className="multi-fields">
           <label>
             Utgångsdatum:
-            <input type="text" placeholder="MM/YY" name="expired" value={formData.expired} onChange={handleChange}></input>
-            {formErrors.expired && <span className="error">{formErrors.expired}</span>}
+            <input
+              type="text"
+              placeholder="MM/YY"
+              name="expired"
+              value={formData.expired}
+              onChange={handleChange}
+            ></input>
+            {formErrors.expired && (
+              <span className="error">{formErrors.expired}</span>
+            )}
           </label>
           <label>
             CVC:
-            <input type="text" placeholder="CVC" name="cvc" value={formData.cvc} onChange={handleChange}></input>
+            <input
+              type="text"
+              placeholder="CVC"
+              name="cvc"
+              value={formData.cvc}
+              onChange={handleChange}
+            ></input>
             {formErrors.cvc && <span className="error">{formErrors.cvc}</span>}
           </label>
           <input type="submit" value="Submit" />
