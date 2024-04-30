a92d84fa9c44155912fbcaad21a0237dc497de17
Peterca85 <peter-carlsson@hotmail.com>
2024-04-30 16:01:46 +0200

    badge shoppingcart

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import ReorderIcon from "@mui/icons-material/Reorder";
 import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import Badge from '@mui/material/Badge';
 import "../styles/Navbar.css";
 
 function Navbar() {
@@ -19,31 +20,36 @@ function Navbar() {
     setIsNavOpen(false);
   };
 
-
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
+  
 
   return (
     <header>
-    <Link to="/">
-      <img src={Logo} width={100} alt="logotype" />
-    </Link>
-    <nav ref={navRef} className={isNavOpen ? "responsive_nav" : ""}>
-      <Link to="/" onClick={closeNavbar}>Hem</Link>
-      <Link to="/meny" onClick={closeNavbar}>Meny</Link>
-      <Link to="/omOss" onClick={closeNavbar}>Om oss</Link>
-      <Link to="/happenings" onClick={closeNavbar}>Happenings</Link>
-      <Link to="/Kontakt" onClick={closeNavbar}>Kontakt</Link>
-      <Link to="/cart" onClick={closeNavbar}>
-        <ShoppingCartIcon fontSize="large"></ShoppingCartIcon>
+      <Link to="/">
+        <img src={Logo} width={100} alt="logotype" />
       </Link>
-      <button className="nav-btn nav-close-btn" onClick={toggleNavbar}>
-        <HighlightOffIcon fontSize="large" />
+      <nav ref={navRef}>
+        <Link to="/"> Hem </Link>
+        <Link to="/meny"> Meny </Link>
+        <Link to="/omOss"> Om oss </Link>
+        <Link to="/happenings"> Happenings </Link>
+        <Link to="/Kontakt"> Kontakt </Link>
+        <Link to="/cart">
+        <Badge badgeContent={4} color="primary">
+          <ShoppingCartIcon fontSize="large"></ShoppingCartIcon>
+          </Badge>
+        </Link>
+        <button className="nav-btn nav-close-btn" onClick={toggleNavbar}>
+          <HighlightOffIcon fontSize="large" />
+        </button>
+      </nav>
+     <button className="nav-btn" onClick={toggleNavbar}>
+        <ReorderIcon fontSize="large" />
       </button>
-    </nav>
-    <button className="nav-btn" onClick={toggleNavbar}>
-      <ReorderIcon fontSize="large" />
-    </button>
-  </header>
-);
+    </header>
+  );
 }
 
 export default Navbar;
